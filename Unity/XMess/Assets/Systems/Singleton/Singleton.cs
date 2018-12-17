87b40df3cbeb8844d3286ed646d90e2585f9fd49
r0w3ntje <r0w3nkuiper@hotmail.com>
2018-12-17 11:08:08 +0100

    - SIngleton system

---
+++ Unity/XMess/Assets/Systems/Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+// Singleton is like making a script accessible like a static class.
+// Singleton can only be used if there's ONE object in the scene with that script. (useful for managers)
+// Inherit the singleton script and acces by typing 'ManagerScript.Instance.*something*'.
+
+namespace Systems.Singleton
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance { get; set; }
+
+        public static T Instance()
+        {
+            instance = FindObjectOfType<T>();
+
+            return instance;
+        }
+    }
+}
