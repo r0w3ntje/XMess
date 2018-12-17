5a271f77c3853189c8d48993b77477507e27a3bd
r0w3ntje <r0w3nkuiper@hotmail.com>
2018-12-17 11:53:30 +0100

    generic rotate script

---
+++ Unity/XMess/Assets/Content/Scripts/Generic/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        transform.Rotate(direction * speed * Time.deltaTime);
+    }
+}
