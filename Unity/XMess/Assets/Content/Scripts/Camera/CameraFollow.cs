bfb6812d97df67198244fcb789c99557235de69b
r0w3ntje <r0w3nkuiper@hotmail.com>
2018-12-18 13:19:22 +0100

    camera follow target script

---
+++ Unity/XMess/Assets/Content/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private bool followX, followY, followZ;
+
+    private Vector3 startCameraPos;
+
+    private void Start()
+    {
+        startCameraPos = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(followX ? target.position.x : startCameraPos.x, followY ? target.position.y : startCameraPos.y, followZ ? target.position.z : startCameraPos.z);
+    }
+}
