3a77ec0141dbf274fd7462f6c938096efd646974
r0w3ntje <r0w3nkuiper@hotmail.com>
2018-12-18 12:00:03 +0100

    - soundmanager - sound instance shizzle

---
+++ Unity/XMess/Assets/Content/Scripts/SoundSystem/SoundInstance.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SoundInstance : MonoBehaviour
+{
+    public AudioSource audioSouce;
+
+    private void FixedUpdate()
+    {
+        if (!audioSouce.isPlaying)
+            Destroy(gameObject);
+    }
+}
---
+++ Unity/XMess/Assets/Content/Scripts/SoundSystem/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using Systems.Singleton;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    [Header("Sound Instance Prefab")]
+    public GameObject soundInstance;
+
+    [Header("Sounds")]
+    public AudioClip monster;
+
+    public void PlayMonsterSound()
+    {
+        PlaySound(monster);
+    }
+
+    public void PlaySound(AudioClip _ac)
+    {
+        GameObject asi = Instantiate(soundInstance);
+        asi.transform.SetParent(transform);
+
+        asi.GetComponent<SoundInstance>().audioSouce.clip = _ac;
+        asi.GetComponent<SoundInstance>().audioSouce.Play();
+    }
+}
