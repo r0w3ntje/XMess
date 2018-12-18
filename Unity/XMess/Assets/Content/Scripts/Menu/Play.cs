c688be3920388d3e60e7a37747b82deabc35db7e
r0w3ntje <r0w3nkuiper@hotmail.com>
2018-12-18 13:53:38 +0100

    play script for main menu

---
+++ Unity/XMess/Assets/Content/Scripts/Menu/Play.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButtonDown("Restart"))
+        {
+            MenuManager.Instance().LoadToScene(1);
+        }
+    }
+}
